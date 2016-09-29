22dede9ab04d4a13ef85f9ac851927c1573fc321
Andy Stoica <andy.stoica@me.com>
2016-09-29 23:16:26 +0100

    Added Movie detail functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
+(function() {
+
+/**
+ * ------------------------------------------------------------------------
+ * Treehouse Full Stack Java Script Techdegree
+ *
+ * Project 5
+ * Build a Movie Search App with the OMDb API
+ * ------------------------------------------------------------------------
+ */
 
 
 // Module variable declaration
 var api = 'http://www.omdbapi.com'; // OMDb API
-
 
 /**
  * ------------------------------------------------------------------------
@@ -19,7 +28,7 @@ function searchMovie(api, searchString, searchYear) {
   return new Promise(function(resolve, reject) {
     // Build query string to include the search string
     // and the optional year if the user provides it
-    var queryString = '/?s=' + searchString + '&r=json';
+    var queryString = '/?s=' + searchString + '&plot=short&r=json';
     if (searchYear) {
       queryString += '&y=' + searchYear;
     }
@@ -63,7 +72,7 @@ function getMovieDetails(api, imdbID) {
   return new Promise(function(resolve, reject) {
     // Build the XHR object and make the call
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', api + '/?i=' + imdbID);
+    xhr.open('GET', api + '/?plot=full&i=' + imdbID);
     xhr.onreadystatechange = handleResponse;
     xhr.onerror = function(err) {
       reject(err);
@@ -97,10 +106,12 @@ function getMovieDetails(api, imdbID) {
  */
 function generateMovieList(results) {
   var html = '';
+  // console.log(results);
   // Iterates over all search results and generates an LI element for each
-  for (var i = 0; i < results.Search.length; i++) {
+  // Limit to maximum 8 results for neater layout
+  for (var i = 0; i < results.Search.length && i < 8; i++) {
     var movie = results.Search[i];
-    html += '<li>' +
+    html += '<li data-imdb="' + movie.imdbID + '">' +
               '<div class="poster-wrap">';
 
     // Append image placeholder if the movie doesn't have a poster
@@ -108,6 +119,7 @@ function generateMovieList(results) {
       html += '<i class="material-icons poster-placeholder">crop_original</i>';
     } else {
       html += '<img class="movie-poster" src="' + movie.Poster + '">';
+      // html += '<img class="movie-poster" src="img/sample.jpg">';
     }
 
     html +=   '</div>' +
@@ -120,10 +132,15 @@ function generateMovieList(results) {
 
 
 /**
- * Updates the page #movies element with a list of LI provided as the argument
+ * Updates the page #movies element with a list of LIs
  */
 function updateMovieList(list) {
   $('#movies').empty().append(list);
+  $('#movies li').click(function(event) {
+    event.preventDefault();
+    var imdbID = $(this).attr('data-imdb');
+    moreDetails(imdbID);
+  });
 }
 
 
@@ -132,26 +149,29 @@ function updateMovieList(list) {
  */
 function displayError(error) {
   var htmlError = '';
+
   // Generate appropriate Error message
+  // If no results were found matching the search string
   if (error.noResults) {
-    // If no results were found matching the search string
     htmlError =
     '<li class="no-movies">' +
     '<i class="material-icons icon-help">help_outline</i>' +
     'No movies found that match: ' + error.search +
     '</li>';
+
+  // If an XHR error occured, display a generic error message
   } else {
-    // If an AJAX error occured
     htmlError =
     '<li class="no-movies">' +
       '<i class="material-icons icon-help">help_outline</i>' +
-      'An error occurred while retrieving the information.' +
+      'An error has occurred while retrieving the information.' +
     '</li>';
   }
 
   // Update the page with the error message
   $('#movies').empty().append(htmlError);
 }
+
 
 ///
 // User Interactivity
@@ -161,12 +181,78 @@ function displayError(error) {
 /**
  * Performs a movie search when the user clicks the search button
  */
- $('#submit').click(function(event) {
-   event.preventDefault();
-   var searchText = $('#search').val();
-   var searchYear = $('#year').val();
-   searchMovie(api, searchText, searchYear)
+$('#submit').click(function(event) {
+  event.preventDefault();
+
+  // Remove any movie details modal window if present
+  $('.movie-details').fadeOut(100).remove();
+  $('#movies').fadeIn(100);
+
+  var searchText = $('#search').val();
+  var searchYear = $('#year').val();
+  searchMovie(api, searchText, searchYear)
       .then(generateMovieList)
       .then(updateMovieList)
       .catch(displayError);
- });
+});
+
+
+/**
+ * Clicking on a movie element, opens up detailed information
+ * about that movie
+ */
+function moreDetails(imdbID) {
+  var htmlLoading = '<div class="movie-details">' +
+                      '<div class="movie-load">' +
+                        '<h2 class="message">Retrieving information...</h2>' +
+                      '</div>' +
+                    '</div>';
+
+
+  // Helper function to generate the movie detail markup
+  function generatePageDetail(movie) {
+
+    var moviePoster = movie.Poster === 'N/A'? 'img/no-media.svg' : movie.Poster;
+    var html = '<div class="movie-close"><a href="#">Search results</a></div>' +
+               '<div class="movie-poster"><img src="' + moviePoster + '" alt=""></div>' +
+               '<div class="movie-info">' +
+                 '<h2><a href="http://imdb.com/title/' + movie.imdbID + '">' + movie.Title + ' (' + movie.Year + ')</a></h2>' +
+                 '<h3>IMDB Rating: ' + movie.imdbRating + '</h3>' +
+                 '<div class="movie-plot">' +
+                   '<h4>Plot synopsis:</h4>' +
+                   '<p>' + movie.Plot + '</p>' +
+                 '</div>' +
+                 '<a href="http://imdb.com/title/' + movie.imdbID + '" class="movie-imdb-link" target="_blank">View on IMDB</a>' +
+               '</div>';
+    return html;
+  }
+
+  // Helper function to display the rendered page detail
+  function updatePageDetail(html) {
+    // Remove the loading message
+    $('.movie-load').remove();
+    $('.movie-details').append($(html).hide().fadeIn(100));
+    $('.movie-close').click(function(event) {
+      event.preventDefault();
+      closeMovieDetails();
+    });
+  }
+
+  // Helper function to close the detail page and bring back the movie list
+  function closeMovieDetails() {
+    $('.movie-details').fadeOut(100).remove();
+    $('#movies').fadeIn(100);
+  }
+
+  // Hide the movie list and display loading message
+  $('#movies').hide();
+  $('body').append(htmlLoading);
+
+  // Make the AJAX call to fetch information for movie ID
+  getMovieDetails(api, imdbID)
+    .then(generatePageDetail)
+    .then(updatePageDetail)
+    .catch(closeMovieDetails);
+}
+
+})();
